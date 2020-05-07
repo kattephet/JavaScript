370f58f51ff62916a3e4dfe9c0c996b8305cfdb9
Kattephet <zielony1990@gmail.com>
2020-05-07 11:28:46 +0100

    practice 1#3

--- praktyka.js
+++ praktyka.js
@@ -33,7 +33,22 @@ let students = [
   track: 'JS, CSS',
   achievments: 773,
   points: 1127
-}
+  },
+   {name: 'greg',
+  track: 'JS, CSS',
+  achievments: 1254,
+  points: 1112
+  },
+   {name: 'anna',
+  track: 'JS, CSS',
+  achievments: 1254,
+  points: 1112
+  },
+   {name: 'tim',
+  track: 'JS, CSS',
+  achievments: 1254,
+  points: 1112
+  }
 ];
 
 let message = ('Type in a name of student you are looking for.');
@@ -41,6 +56,7 @@ message +=(' If you wish to see all the names of our stundents type"list".');
 message +=(' If you wish to quit our program type "quit".');
 let html;
 let search;
+let fulList;
 
 // functions
 
@@ -58,13 +74,23 @@ function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+function printInformation(number) {
+  let print = `<h2>Student: ${capitalizeFirstLetter(students[number].name)}</h2>`;
+  print +=`<p>Track: ${students[number].track}</p>`
+  print +=`<p>Achievments: ${students[number].achievments}</p>`
+  print +=`<p>Points: ${students[number].points}</p>`
+  return print;
+}
+
 // loops
 
 while(true) {
 
   search = prompt(message);
   search = search.toLowerCase();
-  html = "<ol>";
+  fulList = "<h2>List</h2><ol>";
+  html = " ";
+
 
   if ( search === "quit") {
     print('<h1>End</h2>.');
@@ -74,9 +100,16 @@ while(true) {
   for ( let i=0; i<students.length; i+=1) {
 
     if ( search === "list") {
-      html += `${list(i)}`;
+      fulList += `${list(i)}`;
+      html = fulList;
+    } else if ( search === students[i].name ) {
+      html += `${printInformation(i)}`;
     }
-    print(html);
- }
+  }
 
+  if ( html === " ") {
+    html = `<h2>Student ${capitalizeFirstLetter(search)} is not in our base, sorry. Try again.</h2>`;
+  }
+
+ print(html);
 }
