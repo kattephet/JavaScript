c2685d791bc7d20dc780d8a1a1696fb87db2ca98
Kattephet <zielony1990@gmail.com>
2020-05-12 13:12:14 +0100

    small changes

--- contintnts-first-letter.js
+++ contintnts-first-letter.js
@@ -8,12 +8,15 @@ let continents = [
   "Australia",
 ];
 
-let html = '';
+let html = "";
 
 for (i = 0; i < continents.length; i += 1) {
+  // Loop goes through all the index objects
 
-  if ( continents[i].charAt(0) === "A")
-    html += continents[i] ;
+  if (continents[i].charAt(0) === "A")
+    // Checks if there is a name with "A" as a first letter, "chartAt(indexValue)" is a  method which checks for you if there is a certain letter standing in a word at specified index position, in this cause "0" cause you want to check first letter.
+
+    html += continents[i]; // Adds all the index values with "A" as a first letter to 'html'
 }
 
-console.log(html);
+console.log(html); // prints all html to console
