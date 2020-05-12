5e76664575661400a30aea3a5f58b16260d21e69
Kattephet <zielony1990@gmail.com>
2020-05-12 15:32:43 +0100

    removed capitalizeFirstLetter, added toLowerCase as a search compare.

--- praktyka.js
+++ praktyka.js
@@ -1,14 +1,14 @@
 let students = [
-  { name: "greg", track: "Web design, CSS", achievments: 763, points: 122 },
-  { name: "adam", track: "Web design", achievments: 163, points: 422 },
-  { name: "tom", track: "CSS", achievments: 653, points: 152 },
-  { name: "martin", track: "Web design, JS", achievments: 713, points: 162 },
-  { name: "ben", track: "JS, CSS", achievments: 773, points: 1127 },
-  { name: "josh", track: "JS, CSS", achievments: 773, points: 1127 },
-  { name: "rad", track: "JS, CSS", achievments: 773, points: 1127 },
-  { name: "greg", track: "JS, CSS", achievments: 1254, points: 1112 },
-  { name: "anna", track: "JS, CSS", achievments: 1254, points: 1112 },
-  { name: "tim", track: "JS, CSS", achievments: 1254, points: 1112 },
+  { name: "Greg", track: "Web design, CSS", achievments: 763, points: 122 },
+  { name: "Adam", track: "Web design", achievments: 163, points: 422 },
+  { name: "Tom", track: "CSS", achievments: 653, points: 152 },
+  { name: "Martin", track: "Web design, JS", achievments: 713, points: 162 },
+  { name: "Ben", track: "JS, CSS", achievments: 773, points: 1127 },
+  { name: "Josh", track: "JS, CSS", achievments: 773, points: 1127 },
+  { name: "Tad", track: "JS, CSS", achievments: 773, points: 1127 },
+  { name: "Greg", track: "JS, CSS", achievments: 1254, points: 1112 },
+  { name: "Anna", track: "JS, CSS", achievments: 1254, points: 1112 },
+  { name: "Tim", track: "JS, CSS", achievments: 1254, points: 1112 },
 ];
 
 let message = "Type in a name of student you are looking for.";
@@ -37,9 +37,7 @@ function capitalizeFirstLetter(string) {
 }
 
 function printInformation(number) {
-  let print = `<h2>Student: ${capitalizeFirstLetter(
-    students[number].name
-  )}</h2>`;
+  let print = `<h2>Student: ${students[number].name}</h2>`;
   print += `<p>Track: ${students[number].track}</p>`;
   print += `<p>Achievments: ${students[number].achievments}</p>`;
   print += `<p>Points: ${students[number].points}</p>`;
@@ -54,7 +52,7 @@ while (true) {
   fulList = "<h2>List</h2><ol>";
   html = " ";
 
-  if (search === "quit") {
+  if (search === "quit" || search === 'null') {
     print("<h1>End</h2>.");
     break;
   }
@@ -63,7 +61,7 @@ while (true) {
     if (search === "list") {
       fulList += `${list(i)}`;
       html = fulList;
-    } else if (search === students[i].name) {
+    } else if (search === students[i].name.toLowerCase()) {
       html += `${printInformation(i)}`;
     }
   }
