0a66b5035a35d91c0a75ed4a4ae5475278546b8f
Kattephet <zielony1990@gmail.com>
2020-05-14 21:04:10 +0100

    "prettier" + few fixes.

--- multi-object arrays+functions(student_list)1.js
+++ multi-object arrays+functions(student_list)1.js
@@ -1,50 +1,28 @@
 let students = [
-  {name: 'Greg',
-   track: 'Web design, CSS',
-   achievments: 763,
-   points: 122
-  },
-    {name: 'Adam',
-   track: 'Web design',
-   achievments: 163,
-   points: 422
-  },
-    {name: 'Tom',
-   track: 'CSS',
-   achievments: 653,
-   points: 152
-  },
-    {name: 'Martin',
-   track: 'Web design, JS',
-   achievments: 713,
-   points: 162
-  },
-    {name: 'Ben',
-   track: 'JS, CSS',
-   achievments: 773,
-   points: 1127
-  }
+  { name: "Greg", track: "Web design, CSS", achievments: 763, points: 122 },
+  { name: "Adam", track: "Web design", achievments: 163, points: 422 },
+  { name: "Tom", track: "CSS", achievments: 653, points: 152 },
+  { name: "Martin", track: "Web design, JS", achievments: 713, points: 162 },
+  { name: "Ben", track: "JS, CSS", achievments: 773, points: 1127 },
 ];
 
-let html = '';
+let html = "";
 
 function print(message) {
- var outputDiv = document.getElementById('output');
- outputDiv.innerHTML = message;
+  var outputDiv = document.getElementById("output");
+  outputDiv.innerHTML = message;
 }
 
 function printInformation(number) {
   let print = `<h2>Student: ${students[number].name}</h2>`;
-  print +=`<p>Track: ${students[number].track}</p>`
-  print +=`<p>Achievments: ${students[number].achievments}</p>`
-  print +=`<p>Points: ${students[number].points}</p>`
+  print += `<p>Track: ${students[number].track}</p>`;
+  print += `<p>Achievments: ${students[number].achievments}</p>`;
+  print += `<p>Points: ${students[number].points}</p>`;
   return print;
 }
 
-for ( var i = 0 ; i < students.length; i += 1) {
-
+for (var i = 0; i < students.length; i += 1) {
   html += `${printInformation(i)}`;
 }
-
 
 print(html);
--- multi-object arrays+functions(student_list)2.js
+++ multi-object arrays+functions(student_list)2.js
@@ -1,45 +1,23 @@
 let students = [
-  {name: 'greg',
-   track: 'Web design, CSS',
-   achievments: 763,
-   points: 122
-  },
-    {name: 'adam',
-   track: 'Web design',
-   achievments: 163,
-   points: 422
-  },
-    {name: 'tom',
-   track: 'CSS',
-   achievments: 653,
-   points: 152
-  },
-    {name: 'martin',
-   track: 'Web design, JS',
-   achievments: 713,
-   points: 162
-  },
-    {name: 'ben',
-   track: 'JS, CSS',
-   achievments: 773,
-   points: 1127
-  }
+  { name: "greg", track: "Web design, CSS", achievments: 763, points: 122 },
+  { name: "adam", track: "Web design", achievments: 163, points: 422 },
+  { name: "tom", track: "CSS", achievments: 653, points: 152 },
+  { name: "martin", track: "Web design, JS", achievments: 713, points: 162 },
+  { name: "ben", track: "JS, CSS", achievments: 773, points: 1127 },
 ];
 
-
-
-
-
 function print(message) {
- var outputDiv = document.getElementById('output');
- outputDiv.innerHTML = message;
+  var outputDiv = document.getElementById("output");
+  outputDiv.innerHTML = message;
 }
 
 function printInformation(number) {
-  let print = `<h2>Student: ${capitalizeFirstLetter(students[number].name)}</h2>`;
-  print +=`<p>Track: ${students[number].track}</p>`
-  print +=`<p>Achievments: ${students[number].achievments}</p>`
-  print +=`<p>Points: ${students[number].points}</p>`
+  let print = `<h2>Student: ${capitalizeFirstLetter(
+    students[number].name
+  )}</h2>`;
+  print += `<p>Track: ${students[number].track}</p>`;
+  print += `<p>Achievments: ${students[number].achievments}</p>`;
+  print += `<p>Points: ${students[number].points}</p>`;
   return print;
 }
 
@@ -47,28 +25,33 @@ function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-let html = '';
+let html = "";
 let search;
 let result;
 
 while (true) {
-
-  search = prompt('Type in student name you want to look for.(Type in "quit" if you want to End).');
+  search = prompt(
+    'Type in student name you want to look for.(Type in "quit" if you want to End).'
+  );
   search = search.toLowerCase();
 
-if (search === 'quit' || search === 'null' ) {
-  break;
+  if (search === "quit" || search === "null") {
+    break;
   }
 
-  for ( var i = 0; i < students.length; i += 1 ) {
-
+  for (var i = 0; i < students.length; i += 1) {
     let student = students[i].name;
-    if ( student === search) {
+    if (student === search) {
       result = i;
       html += printInformation(i);
       print(html);
-    } else if ( html === '' ) {
-      print(`<h2>Student ${capitalizeFirstLetter(search)} is not in our base, sorry. Try again.</h2>`);
+    } else if (html === "") {
+      print(
+        `<h2>Student ${capitalizeFirstLetter(
+          search
+        )} is not in our base, sorry. Try again.</h2>`
+      );
     }
-  }  html = '';
+  }
+  html = "";
 }
