0a43606889836e0512dd21a7d42e1d16a233fd0d
Kattephet <zielony1990@gmail.com>
2020-05-14 14:14:24 +0100

    "prettier"

--- click-count/counter.js
+++ click-count/counter.js
@@ -1,50 +1,50 @@
-const button = document.querySelector('button.resetCounter');
-const clickArea = document.querySelector('html:not(.resetCounter)');
-const counter = document.querySelector('#main-heading');
-const scores = document.querySelector('.scores');
-const motivator = document.querySelector('.motivator')
+const button = document.querySelector("button.resetCounter");
+const clickArea = document.querySelector("html:not(.resetCounter)");
+const counter = document.querySelector("#main-heading");
+const scores = document.querySelector(".scores");
+const motivator = document.querySelector(".motivator");
 let clicks = 1;
 let listPosition = 1;
 
 function comment(clicksAmount) {
-  if ( clicksAmount < 10 ) {
-    return 'poor.';
-  } else if ( clicksAmount < 40 ) {
-    return 'not bad';
-  } else if ( clicksAmount < 100 ) {
-    return 'nice!';
+  if (clicksAmount < 10) {
+    return "poor.";
+  } else if (clicksAmount < 40) {
+    return "not bad";
+  } else if (clicksAmount < 100) {
+    return "nice!";
   } else {
-    return 'WOW!';
+    return "WOW!";
   }
 }
 
 function motivatorTitle(clicksAmount) {
-  if ( clicks < 10) {
+  if (clicks < 10) {
     motivator.textContent = "Lets go!";
-  } else if ( clicks < 50) {
+  } else if (clicks < 50) {
     motivator.textContent = "You are doing greate!";
-  } else if ( clicks < 80) {
+  } else if (clicks < 80) {
     motivator.textContent = "WOW!!!";
     motivator.style.fontSize = "70px";
-    motivator.style.color = "red"
-  } else if ( clicks < 100) {
+    motivator.style.color = "red";
+  } else if (clicks < 100) {
     motivator.textContent = "Keep going, don't stop!";
   } else {
     motivator.textContent = "UNBELIEVALBE!!!!";
     motivator.style.fontSize = "100px";
-}
+  }
 }
 
-clickArea.addEventListener('click', () => {
+clickArea.addEventListener("click", () => {
   counter.textContent = `Click nr: ${clicks}`;
   clicks += 1;
   motivatorTitle(clicks);
 });
 
-button.addEventListener('click', () => {
-  let li = document.createElement('li');
+button.addEventListener("click", () => {
+  let li = document.createElement("li");
   li.textContent = `${listPosition}: ${clicks - 1} clicks - ${comment(clicks)}`;
   scores.appendChild(li);
   clicks = 0;
   listPosition += 1;
-})
+});
