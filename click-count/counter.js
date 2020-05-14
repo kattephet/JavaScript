59de18d7d1501900a45ab9eac23ef0dc53ab4d78
Kattephet <zielony1990@gmail.com>
2020-05-14 20:43:59 +0100

    small changes to layout, added alert after 3s.

--- click-count/counter.js
+++ click-count/counter.js
@@ -1,8 +1,9 @@
-const button = document.querySelector("button.resetCounter");
+const resetButton = document.querySelector("button.resetCounter");
 const clickArea = document.querySelector("html:not(.resetCounter)");
 const counter = document.querySelector("#main-heading");
 const scores = document.querySelector(".scores");
 const motivator = document.querySelector(".motivator");
+let timeoutID;
 let clicks = 1;
 let listPosition = 1;
 
@@ -25,7 +26,7 @@ function motivatorTitle(clicksAmount) {
     motivator.textContent = "You are doing greate!";
   } else if (clicks < 80) {
     motivator.textContent = "WOW!!!";
-    motivator.style.fontSize = "70px";
+    motivator.style.fontSize = "75px";
     motivator.style.color = "red";
   } else if (clicks < 100) {
     motivator.textContent = "Keep going, don't stop!";
@@ -36,15 +37,18 @@ function motivatorTitle(clicksAmount) {
 }
 
 clickArea.addEventListener("click", () => {
+  window.clearTimeout(timeoutID);
   counter.textContent = `Click nr: ${clicks}`;
   clicks += 1;
   motivatorTitle(clicks);
+  timeoutID = window.setTimeout(window.alert, 3*1000, 'Are you done? COME ON!');
 });
 
-button.addEventListener("click", () => {
+resetButton.addEventListener("click", () => {
   let li = document.createElement("li");
   li.textContent = `${listPosition}: ${clicks - 1} clicks - ${comment(clicks)}`;
   scores.appendChild(li);
   clicks = 0;
+  motivator.style.color = "black";
   listPosition += 1;
 });
