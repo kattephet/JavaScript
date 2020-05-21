8380a81d6fa0326c6814ad016ffaf1c148171e91
Kattephet <zielony1990@gmail.com>
2020-05-21 18:27:23 +0100

    Changed Score board, added high->low filter, and colored bg. CSS changes

--- click-count/counter.js
+++ click-count/counter.js
@@ -6,6 +6,20 @@ const motivator = document.querySelector(".motivator");
 let timeoutID;
 let clicks = 1;
 let listPosition = 1;
+let listElement = scores.children;
+
+let colors = [
+  "#C2272D",
+  "#ff5900",
+  "#F8931F",
+  "#FFFF01",
+  "#c4f04a",
+  "#009245",
+  "#0193D9",
+  "#017bd9",
+  "#0C04ED",
+  "#612F90",
+];
 
 function comment(clicksAmount) {
   if (clicksAmount < 10) {
@@ -43,6 +57,34 @@ function addRemoveButton(li) {
   li.appendChild(remove);
 }
 
+function sortPosition(score) {
+  while (true) {
+    let newScore = parseInt(score.className);
+    let compareScore = parseInt(score.previousElementSibling.className);
+    if (newScore > compareScore) {
+      let up = score;
+      let down = score.previousElementSibling;
+      scores.insertBefore(up, down);
+    } else {
+      break;
+    }
+  }
+}
+
+function removeOverTen() {
+  let listLength = scores.childElementCount;
+  let lastScore = scores.lastElementChild;
+  if (listLength > 11) {
+    scores.removeChild(lastScore);
+  }
+}
+
+function backgroundColors() {
+  for (let i = 1; i < 11; i += 1) {
+    listElement[i].style.backgroundColor = colors[i - 1];
+  }
+}
+
 clickArea.addEventListener("click", () => {
   window.clearTimeout(timeoutID);
   counter.textContent = `Click nr: ${clicks}`;
@@ -54,11 +96,15 @@ clickArea.addEventListener("click", () => {
 resetButton.addEventListener("click", () => {
   let li = document.createElement("li");
   li.innerHTML = `${listPosition}: ${clicks - 1} clicks - ${comment(clicks)}`;
+  li.className = `${clicks - 1}`;
   scores.appendChild(li);
   addRemoveButton(li);
+  sortPosition(li);
+  removeOverTen();
   clicks = 0;
   motivator.style.color = "black";
   listPosition += 1;
+  backgroundColors();
 });
 
 scores.addEventListener("click", (event) => {
