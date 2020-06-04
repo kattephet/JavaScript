58979fab8fd7f4710bb5321d7d96413f2ce299d7
Kattephet <zielony1990@gmail.com>
2020-06-04 17:02:14 +0100

    added filter() method instead of loop.

--- contintnts-first-letter.js
+++ contintnts-first-letter.js
@@ -8,15 +8,34 @@ let continents = [
   "Australia",
 ];
 
-let html = "";
+let html = continents.filter(countryName => {
+  if (countryName.charAt(0) === "A") {
+    return countryName;
+  }
+});
 
-for (i = 0; i < continents.length; i += 1) {
-  // Loop goes through all the index objects
+console.log(html);
 
-  if (continents[i].charAt(0) === "A")
-    // Checks if there is a name with "A" as a first letter, "chartAt(indexValue)" is a  method which checks for you if there is a certain letter standing in a word at specified index position, in this cause "0" cause you want to check first letter.
+// function aFilter (countryName) {
+//   if (countryName.charAt(0) === "A") {
+//     return countryName;
+//   }
+// };
+//
+// let html = continents.filter(aFilter);
+// console.log(html);
 
-    html += continents[i]; // Adds all the index values with "A" as a first letter to 'html'
-}
+//Old version
 
-console.log(html); // prints all html to console
+// let html = "";
+//
+// for (i = 0; i < continents.length; i += 1) {
+//   // Loop goes through all the index objects
+//
+//   if (continents[i].charAt(0) === "A")
+//     // Checks if there is a name with "A" as a first letter, "chartAt(indexValue)" is a  method which checks for you if there is a certain letter standing in a word at specified index position, in this cause "0" cause you want to check first letter.
+//
+//     html += continents[i]; // Adds all the index values with "A" as a first letter to 'html'
+// }
+//
+// console.log(html); // prints all html to console
